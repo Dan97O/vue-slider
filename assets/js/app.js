07faf146a75c7531085f7198b9b2f03982a747f4
Dan97O <dan97-oddo@live.it>
2023-03-29 15:28:15 +0200

    add infinity loop prev

--- assets/js/app.js
+++ assets/js/app.js
@@ -50,6 +50,12 @@ createApp({
       if (this.activeImage === this.images.length) {
         this.activeImage = 0
       }
+    },
+    prev(){
+      this.activeImage--
+      if (this.activeImage < 0) {
+        this.activeImage = this.images.length -1
+    }
     }
   }
 }).mount('#app')
